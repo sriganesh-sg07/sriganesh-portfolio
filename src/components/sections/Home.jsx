b765dde5e3a77125a4fac72bf17e66bf8df3a5f8
Sriganesh N <sriganeshn.cse@citchennai.net>
2025-04-15 17:46:57 +0530

    second commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -28,7 +28,7 @@ export const Home = () =>{
                         </a>
 
                         <a
-                            href="#projects"
+                            href="#contact"
                             className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
                             hover:-translate-y-0.5 
                             hover:shadow-[0_0_15px_rgba(59,130,246,0.2)]
