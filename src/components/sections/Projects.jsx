c73d359f2a0d11640d9f48a87daa592e7e56ba00
Sriganesh N <sriganeshn.cse@citchennai.net>
2025-04-15 17:58:18 +0530

    second commit

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -16,12 +16,12 @@ export const Projects = () =>{
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+                    <h3 className="text-xl font-bold mb-2">Banking Sector</h3>
                             <p className="text-gray-400 mb-4">
-                            Car Service Crud Web Application 
+                            Mobile Banking App Development
                             </p>
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React.js", "Spring Boot", "Junit", "Mysql", "Jenkins", "Docker"].map((tech,key) =>(
+                                {["Java", "Retrofit", "Sqlite", "XML", "Spring MVC", "Git"].map((tech,key) =>(
                                     <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -40,7 +40,7 @@ export const Projects = () =>{
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-{0_2px_8px_rgba(59,130,246,0.1)] transition">
-                            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+                            <h3 className="text-xl font-bold mb-2">Backend</h3>
                             <p className="text-gray-400 mb-4">
                             Car Service Crud Web Application 
                             </p>
@@ -64,12 +64,12 @@ export const Projects = () =>{
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-{0_2px_8px_rgba(59,130,246,0.1)] transition">
-                            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+                            <h3 className="text-xl font-bold mb-2">Reservation Platform</h3>
                             <p className="text-gray-400 mb-4">
-                            Car Service Crud Web Application 
+                            Flight Ticket Reservation
                             </p>
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React.js", "Spring Boot", "Junit", "Mysql", "Jenkins", "Docker"].map((tech,key) =>(
+                                {["React.js", "Node.js", "Express.js", "MongoDB", "Git", "Docker"].map((tech,key) =>(
                                     <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -87,29 +87,7 @@ export const Projects = () =>{
                             </div>
                         </div>
 
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-{0_2px_8px_rgba(59,130,246,0.1)] transition">
-                            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
-                            <p className="text-gray-400 mb-4">
-                            Car Service Crud Web Application 
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["React.js", "Spring Boot", "Junit", "Mysql", "Jenkins", "Docker"].map((tech,key) =>(
-                                    <span
-                                    key={key}
-                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                                hover:shadow-{0_2px_8px_rgba(59,130,246,0.1)] transition-all"
-                                >
-                                    {tech}
-                                </span>
-                                ))}
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
-                                    {" "}
-                                    View Project → {" "}
-                                </a>
-                            </div>
-                        </div>
+                      
 
                     </div>
 
